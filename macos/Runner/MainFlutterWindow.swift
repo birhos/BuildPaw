d8bdeccbaff272678cb4829a1705b606aa8b2b0b
Mustafa TÜRKMEN <mustafa.turkmen@onedose.io>
2026-02-25 13:40:17 +0300

    feat(): add macOS support and dependencies

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -8,6 +8,9 @@ class MainFlutterWindow: NSWindow {
     self.contentViewController = flutterViewController
     self.setFrame(windowFrame, display: true)
 
+    self.minSize = NSSize(width: 1000, height: 700)
+    self.title = "BuildPaw"
+
     RegisterGeneratedPlugins(registry: flutterViewController)
 
     super.awakeFromNib()
